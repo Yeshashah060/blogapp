75ab4f722cc1cda81cbce2fbd4c1810592914f28
Yeshashah060 <139575396+Yeshashah060@users.noreply.github.com>
2023-08-26 00:25:53 +0530

    Add files via upload

---
+++ blogging/blogginapp/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpRequest
+from.models import blog
+# Create your views here.
+def frontpage(request):
+	return render(request,'front.html')
+
+def homepage(request):
+	return render(request,'home.html')
+
+def addblog(request):
+	return render(request,'addblog.html')
+
+def savedata(request):
+	bname = request.GET.get("bname")
+	bb = request.GET.get("bb")
+
+	b=blog(bname=bname,bb=bb)
+	b.save()
+
+	data = blog.objects.all()
+	return render(request,'displayblog.html',{'data':data})
+
+
+def displayblog(request):
+	data = blog.objects.all()
+	return render(request,'displayblog.html',{'data':data})
+
+
+
+
+
+
+
+
+# def deleteblog(request):
+# 	bname=request.GET.get('bname')
+# 	data = blog.objects.get(bname=bname)
+# 	data.delete()
+# 	data = blog.object.all()
+# 	return render(request,'displayblog.html',{'data':data})
+
+# def editblog(request):
+# 	data = blog.objects.all()
+# 	return render(request,'editblog.html',{'data':data})
+
+# def edit(request):
+# 	return render(request,"edit.html")
+
